aba950b65eb7c97527571fdadc8c1f482a71f9d6
cloudpc7 <cloudp7@hotmail.com>
2021-03-05 16:07:25 -0800

    create html & CSS

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*----- constants -----*/
+const buttons = {
+    0: 'blue',
+    1: 'red',
+    2: 'yellow',
+    3: 'green'
+}
+/*----- app's state (variables) -----*/
+const computer, player, start, reset, timer
+/*----- cached element references -----*/
+const sequence = {
+    'blue': document.querySelector('blue'),
+    'red': document.querySelector('red'),
+    'yellow': document.querySelector('yellow'),
+    'green': document.querySelector('green')
+}
+computer = [];
+player = [];
+/*----- event listeners -----*/
+/*----- functions -----*/
+function init(){
+
+}
+init();
+
+function render(){
+
+};
+
+function simon(){
+
+}
