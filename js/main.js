6773cca82110edbb074d12b2e8ffe617b1a4585c
cloudpc7 <cloudp7@hotmail.com>
2021-03-07 07:42:18 -0800

    create player function

--- js/main.js
+++ js/main.js
@@ -6,13 +6,27 @@
      3: 'green'
  }
  /*----- app's state (variables) -----*/
- let computer, player, reset, time, start
+ let computer, player, reset, time, start;
  /*----- cached element references -----*/
 time = 3;
+computer = {
+    0: document.getElementById("red"),
+    1: document.getElementById("blue"),
+    2: document.getElementById("yellow"),
+    3: document.getElementById("green")
+}
+player = {
+    0: document.getElementById("red"),
+    1: document.getElementById("blue"),
+    2: document.getElementById("yellow"),
+    3: document.getElementById("green")
+}
  /*----- event listeners -----*/
  document.getElementById("start").addEventListener("click",starter);
  //document.getElementById("reset").addEventListener("click",restart);
+ 
 /*----- functions -----*/
+init();
 function starter(e){
     let start = setInterval(function(){
         if(time <=0){
@@ -26,14 +40,23 @@ function starter(e){
     },3000);
 }
 
-// function restart(e){
-//     starter
-// }
-function init(){
+function computeSequence(){
+    for(let i in computer){
 
+    }
 }
-init();
+computeSequence();
 
-function render(){
+function playerSequence(){
+    for(let i in player){
+        player[i].addEventListener("click",play);
+        function play(e){
+            console.log(player[i]);
+        }
+    }
+    
+}
+playerSequence();
+function init(){
 
 }
