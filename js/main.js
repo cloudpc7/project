b6f0fcae37fd7401d4794cd49096bd24d075dfe0
cloudpc7 <cloudp7@hotmail.com>
2021-03-08 13:21:19 -0800

    while loop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 /*----- constants -----*/
+const duration = 1800; // show code duration
+const gap = 600; // time between  codes
 const colors = {
     0: document.getElementById("red"),
     1: document.getElementById("blue"),
@@ -6,50 +8,78 @@ const colors = {
     3: document.getElementById("green")
 }
  /*----- app's state (variables) -----*/
- let  start, timer, win, turn, time, randomSequence, computerSequence, playerSequence;
+ let isPlayersTurn, randomSequence, playerSequence, gameOver;
+
  /*----- cached element references -----*/
- time = 3;
-computer = {
-    0: "red",
-    1: "blue",
-    2: "yellow",
-    3: "green"
-}
-player = {
-    0: "red",
-    1: "blue",
-    2: "yellow",
-    3: "green"
-}
+const msgEl = document.getElementById("msg");
+const startBtn = document.getElementById("start");
+const colorBtns = document.querySelectorAll("#board > button");
+ 
+/*----- event listeners -----*/
+startBtn.addEventListener("click", handleStartClick);
+document.getElementById("board").addEventListener("click", handleColorClick);
 
-computerSequence = [];
-playerSequence = [];
-turn = null;
-
- /*----- event listeners -----*/
- //document.getElementById("start").addEventListener("click",starter());
- document.querySelectorAll("button").addEventListener("click",play);
 /*----- functions -----*/
 init();
-
-// function computeSequence(){
-//     if(turn === null){
-//         randomSequence = Math.floor(Math.random() * (Object.keys(computer).length) +1);
-//     }
-//     const sequence = setTimeout(function (){
-//         computerSequence.push(randomSequence);
-//     },800);
-//     clearTimeout(sequence);
-// }
-// computeSequence();
-
-function play(e){
-    console.log(e.target);
+function handleStartClick(){
+    if(!gameOver || isPlayersTurn) return;
+    gameOver = false;
+    isPlayersTurn = false;
+    randomSequence = [];
+    render();
+    // while (!gameOver){
+        //computer sequenece picks random number.
+        randomSequence.push(Math.floor(Math.random() * 4));
+        console.log(randomSequence);
+        render();
+        //render computer sequence.
+        renderSequence(function (){
+            isPlayersTurn = true;
+            playerSequence = [];
+            render();
+        });
+    // }
 }
-function render(){
+function renderSequence(cb){
+    let codeIdx = 0;
+    let timerId = setInterval(function(){
+        if(codeIdx === randomSequence.length){
+            clearInterval(timerId);
+            return cb();
+        } else {
+            const code = randomSequence[codeIdx];
+            //display button as on
+            colorBtns[code].classList.add("light");
+            setTimeout(function (){
+                colorBtns[code].classList.remove("light");
+                codeIdx++;
+            }, duration);
+        }
+    }, duration + gap);
+}
+function handleColorClick(e){
+    if(e.target.id === "board" || gameOver || !isPlayersTurn ) return;
+    //what is the state to update and test if player just lost
+    // if gameover true show player loss
+}
 
+function render(){
+    startBtn.style.visibility = gameOver  ? "visible" : "hidden";
+    if(gameOver && randomSequence.length === 0){
+        msgEl.innerHTML = "Click Start To Play!";
+    } else if(gameOver) {
+        msgEl.innerHTML = "Game Over!";
+    } else if (isPlayersTurn){
+        msgEl.innerHTML = `LEVEL ${randomSequence.length} - Your Turn!`;
+    } else {
+        msgEl.innerHTML = `LEVEL ${randomSequence.length} - Computer's Turn!`;
+    }
 }
 
 function init(){
-
+   // for futre icebox
+   randomSequence = [];
+   gameOver = true;
+   isPlayersTurn = false;
+   render();
 }
